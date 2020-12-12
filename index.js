0b966f37a535151ed97b7395838fa1c123d13f6d
André Pedro <32388352+manzarra@users.noreply.github.com>
2020-12-12 09:42:05 +0000

    first app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //teste primeiro commit
+//console.log('Hello World');
+
+const { write } = require('fs');
+var http = require('http');
+var server = http.createServer(function(req,res){
+    res.writeHead(200);
+    res.end('Hello World - Teste');
+});
+server.listen(8000);
